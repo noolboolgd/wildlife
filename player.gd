b985822bacf0b28ccd2da55da0ac1635bc601f35
noolboolgd <noolbolgd@gmail.com>
2026-09-12 15:28:45 +0200

    Basic player controller

---
+++ player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.001
+
+var look_dir := Vector2.ZERO
+
+@onready var camera: Camera3D = $Camera3D
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		look_dir = event.relative * MOUSE_SENSITIVITY
+		rotate_camera()
+
+
+func rotate_camera() -> void:
+	rotate_y(-look_dir.x) #Look horizontally
+	camera.rotation.x -= look_dir.y #Look Up'n'Down
+	camera.rotation.x = clamp(camera.rotation.x, -PI / 2, PI / 2)
